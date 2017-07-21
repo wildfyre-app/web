63d244083b11d516b872adc1292d488d24069f81
Info-Screen <Info-Screen@Info-Screen.me>
2017-07-21 20:31:25 +0200

    Send pageviews to analytics
    Summary: Setup Angulartics
    
    Test Plan: Request Page and see check with piwik if tracked
    
    Reviewers: WyldBot
    
    Reviewed By: WyldBot
    
    Differential Revision: https://phabricator.wildfyre.net/D31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
+import { Angulartics2Piwik } from 'angulartics2';
 ﻿import { Component } from '@angular/core';
+
+
 @Component({
   selector: 'app-component',
   templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(
+    angulartics2Piwik: Angulartics2Piwik
+  ) { }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Angulartics2Module, Angulartics2Piwik } from 'angulartics2';
 import {MdButtonModule, MdCheckboxModule, MdTabsModule, MdCardModule,
   MdMenuModule, MdInputModule, MdListModule, MdSlideToggleModule} from '@angular/material';
 import 'hammerjs';
@@ -41,7 +42,9 @@ import { ProfileViewComponent } from './profileView/index';
         [MdInputModule],
         [MdListModule],
         [MdSlideToggleModule],
-        [ReCaptchaModule]
+        [ReCaptchaModule],
+
+        Angulartics2Module.forRoot([ Angulartics2Piwik ])
     ],
     declarations: [
         AppComponent,
