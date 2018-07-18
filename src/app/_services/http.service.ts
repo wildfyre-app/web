wildfyre-app/web

commit 4a7948365433214f775266a8bc526389583cd3f1

Angular 5
Summary:
>*Delete node_modules and package-lock.json*
>*Run `npm install`*
Based on D158
The long awaited Angular 5!

Removes prismjs as we dont use it
Updates from angular 4.4.4 to 5.2.11
Updates material to latest compatible and changes all md tags to mat
Updates typescript version
Adds workaround for navbar back button, due to new navbar styling
Updates http and httpmodule to their new respective objects; httpclient, and httpclientmodule
Response is now a json object

Updates request headers

Implements ng2share as an internal module
Fixes T149

Test Plan: Test all functionality

Reviewers: Info-Screen, CLOVIS

Reviewed By: Info-Screen

Subscribers: CLOVIS, Info-Screen, WyldBot

Maniphest Tasks: T149, T122

Differential Revision: https://phabricator.wildfyre.net/D163

src/app/404component/404.component.html
@@ -1,7 +1,8 @@
 <a routerLink="/" routerLinkActive="active"><img class="logo" src="https://static.wildfyre.net/wildfyre.png" alt="WildFyre"></a>
-<md-card class="example-card">
-  <md-card>
+<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
+<mat-card class="example-card">
+  <mat-card>
     <h1>404?<br>
     We do not know what you are looking for. You could possibly start <a routerLink="/login" routerLinkActive="active">here</a> first.</h1>
-  </md-card>
-</md-card>
+  </mat-card>
+</mat-card>

src/app/_services/http.service.ts
@@ -1,7 +1,7 @@
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { isDevMode } from '@angular/core';
-import { MdSnackBar } from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthenticationService } from './authentication.service';
@@ -11,9 +11,9 @@ export class HttpService {
   private apiURL: string;
 
   constructor(
-    private http: Http,
+    private http: HttpClient,
     private router: Router,
-    private snackBar: MdSnackBar,
+    private snackBar: MatSnackBar,
     private authenticationService: AuthenticationService
   ) {
     this.apiURL = 'https://api.wildfyre.net';
@@ -142,34 +142,12 @@ export class HttpService {
     return Observable.throw(error);
   }
 
-  getOptions(headers?: Headers|null): RequestOptions {
-    headers = headers || new Headers();
-
-    // Set Content-Type to JSON if not already set
-    if (!headers.has('Content-Type')) {
-      headers.append('Content-Type', 'application/json');
-    }
-
-    // Add Token if available
-    if (this.authenticationService.token != null) {
-      headers.append('Authorization', 'Token ' + this.authenticationService.token);
-    }
-
-    return new RequestOptions({
-      headers: headers
-    });
+  getOptions(): any {
+    return { headers: new HttpHeaders({ 'Content-Type': 'application/json',
+     'Authorization': 'Token ' + this.authenticationService.token}) };
   }
 
-  getOptionsForImage(headers?: Headers|null): RequestOptions {
-    headers = headers || new Headers();
-
-    // Add Token if available
-    if (this.authenticationService.token != null) {
-      headers.append('Authorization', 'Token ' + this.authenticationService.token);
-    }
-
-    return new RequestOptions({
-      headers: headers
-    });
+  getOptionsForImage(): any {
+    return { headers: new HttpHeaders({ 'Authorization': 'Token ' + this.authenticationService.token}) };
   }
 }

src/app/_services/registration.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { MdSnackBar } from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 import { Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Registration, RegistrationError } from '../_models/registration';
@@ -10,7 +10,7 @@ import { HttpService } from './http.service';
 @Injectable()
 export class RegistrationService {
   constructor(
-    private snackBar: MdSnackBar,
+    private snackBar: MatSnackBar,
     private httpService: HttpService
   ) { }
 
@@ -24,7 +24,7 @@ export class RegistrationService {
     return this.httpService.POST('/account/recover/', body)
       .map((response: Response) => {
         // Step 1 successful
-        return RecoverTransaction.parse(response.json());
+        return RecoverTransaction.parse(response);
       })
       .catch((error) => {
         return Observable.of(
@@ -48,7 +48,7 @@ export class RegistrationService {
     return this.httpService.POST('/account/recover/reset/', body)
       .map((response: Response) => {
         // Password reset successful
-        return Reset.parse(response.json());
+        return Reset.parse(response);
       })
       .catch((error) => {
         return Observable.of(
@@ -72,7 +72,7 @@ export class RegistrationService {
     return this.httpService.POST('/account/recover/', body)
       .map((response: Response) => {
         // Usernames sent successfully
-        return RecoverTransaction.parse(response.json());
+        return RecoverTransaction.parse(response);
       })
       .catch((error) => {
         return Observable.of(
@@ -97,7 +97,7 @@ export class RegistrationService {
     return this.httpService.POST('/account/register/', body)
       .map((response: Response) => {
         // Registration successful
-        return Registration.parse(response.json());
+        return Registration.parse(response);
       })
       .catch((error) => {
         return Observable.of(

commit 497b23a6ad609fdd5dcb9d68254626f7d78d3658

Bugfixes for 4a7948365433
Summary:
Fixes bugs introduced in 4a7948365433 (D163)

Fixes people being unable to register
Fixes login logic, instead of freezing on incorrect credentials we now respond with a generic error message.

Fixes a lot of minor bugs:
Fixes asterisks on input fields
Adds Open Source link to side navbar
Implements somewhat of a fix for flashing back button (Still performs slowly on post view)
Fixes bottom navbar highlight(sometimes the highlight was on the wrong section(or not working))
Fixes notification archive and user posts not taking into account its main image(for image icon)
Fixes weird offset background color
Fixes 2px space between navbar and top navbar(desktop only)
Removes odd highlight to the left of pictures(desktop only)
Fully center aligns area drop down
Changes pointer cursor on back button to better reflect its use
Fixes offset comment send button(desktop only)

Fixes T163, T203

Test Plan: Look at fixes?

Reviewers: Info-Screen, CLOVIS

Reviewed By: Info-Screen

Subscribers: CLOVIS, WyldBot, Info-Screen

Maniphest Tasks: T203, T163

Differential Revision: https://phabricator.wildfyre.net/D179

src/app/404component/404.component.html
@@ -1,5 +1,4 @@
 <a routerLink="/" routerLinkActive="active"><img class="logo" src="https://static.wildfyre.net/wildfyre.png" alt="WildFyre"></a>
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="example-card">
   <mat-card>
     <h1>404?<br>

src/app/_services/http.service.ts
@@ -58,6 +58,12 @@ export class HttpService {
       .catch((error: any) => this.handleError(error));
   }
 
+  POST_REGISTER(passedUrl: string, body: any): Observable<any> {
+    // POST to api
+    return this.http.post(this.apiURL + passedUrl, body, this.getOptionsForRegister())
+      .catch((error: any) => this.handleError(error));
+  }
+
   PUT(passedUrl: string, body: any): Observable<any> {
     // PUT to api
     return this.http.put(this.apiURL + passedUrl, JSON.stringify(body), this.getOptions())
@@ -149,5 +155,9 @@ export class HttpService {
 
   getOptionsForImage(): any {
     return { headers: new HttpHeaders({ 'Authorization': 'Token ' + this.authenticationService.token}) };
+  }
+
+  getOptionsForRegister(): any {
+    return { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
   }
 }

src/app/_services/registration.service.ts
@@ -94,7 +94,7 @@ export class RegistrationService {
       'captcha': captchaResponse
     };
 
-    return this.httpService.POST('/account/register/', body)
+    return this.httpService.POST_REGISTER('/account/register/', body)
       .map((response: Response) => {
         // Registration successful
         return Registration.parse(response);

src/app/createPost/createPost.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="example-card" (click)="loadDrafts()">
   <button class="wideButtons" mat-button><i class="material-icons">folder</i> Drafts</button>
 </mat-card>

src/app/home/home.component.html
@@ -5,7 +5,6 @@
   <br><br>
   <button class="wideButtons" mat-button (click)="ngOnInit()">Refresh <i class="material-icons">refresh</i></button>
 </div>
-  <div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
   <br>
   <mat-card class="example-card" *ngIf="post && !loading">
     <a *ngIf="post?.image" href="{{post?.image}}"><img src="{{post?.image}}" /></a>
@@ -64,7 +63,7 @@
 
 <mat-card class="refreshButton" (click)="refresh(false)" *ngIf="(commentCount === 0) && !loading && post.id !== 0">
     No comments, try commenting?
-    <button class="wideButtons" *ngIf="!loading" mat-button>Refresh <i class="material-icons">refresh</i></button>
+    <button class="wideButtons" mat-button>Refresh <i class="material-icons">refresh</i></button>
 </mat-card>
 <br *ngIf="(post.id !== 0) && !loading">
 <br *ngIf="(post.id !== 0) && !loading">

src/app/login/login.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="login-card">
   <a class="logoLink" routerLink="/" routerLinkActive="active"><img class="logo" src="https://static.wildfyre.net/wildfyre.png" alt="WildFyre"></a>
   <mat-card-content>
@@ -8,10 +7,8 @@
     </div>
     <h2>Login</h2>
     <form name="form" (ngSubmit)="f.form.valid && login()" #f="ngForm" novalidate>
-      <div *ngIf="errors && errors.non_field_errors" class="alert alert-danger">
-        <ul *ngFor="let err of errors.non_field_errors">
-          <li>{{err}}</li>
-        </ul>
+      <div *ngIf="errors" class="alert alert-danger">
+        {{errors}}
       </div>
 
       <div *ngIf="!loading" class="form" [ngClass]="{ 'has-error': f.submitted && !username.valid }">
@@ -32,7 +29,7 @@
         <button *ngIf="!loading" class="wideButtons" mat-raised-button color="accent" [disabled]="loading"><b>Login</b></button>
         <br>
         <mat-progress-spinner *ngIf="loading"></mat-progress-spinner>
-        </div>
+      </div>
     </form>
     Can't login? <a routerLink="/recover" routerLinkActive="active">Recover it Here</a><br>
     Don't have an account? <a routerLink="/register" routerLinkActive="active">Register Here</a>

src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
+import 'rxjs/add/operator/catch';
 import { Subject } from 'rxjs/Subject';
 import { AuthError } from '../_models/auth';
 import { AuthenticationService } from '../_services/authentication.service';
@@ -13,7 +14,7 @@ import { RouteService } from '../_services/route.service';
 })
 export class LoginComponent implements OnInit, OnDestroy {
   componentDestroyed: Subject<boolean> = new Subject();
-  errors: AuthError;
+  errors: string;
   loading = false;
   model: any = {};
 
@@ -58,10 +59,10 @@ export class LoginComponent implements OnInit, OnDestroy {
           this.navBarService.areaVisible.next(true);
           this.router.navigate(['/']);
           this.loading = false;
-        } else {
-          this.errors = result.getError();
-          this.loading = false;
         }
+    }, err  => {
+      this.errors = 'Unable to log in with provided credentials.';
+      this.loading = false;
     });
   }
 }

src/app/navBar/navBar.component.html
@@ -1,5 +1,5 @@
-<nav id="topNav" mat-tab-nav-bar aria-label="Navigation links">
-  <mat-select class="areaSelect" *ngIf="areaVisible" [(ngModel)]="currentArea" (ngModelChange)="onChange($event)">
+<nav [style.width]="width" id="topNav" mat-tab-nav-bar aria-label="Navigation links">
+  <mat-select id="areaSelect" *ngIf="areaVisible" [(ngModel)]="currentArea" (ngModelChange)="onChange($event)">
     <mat-option *ngFor="let area of areas" [value]="area.name">{{area.getDisplayName()}} - {{area.spread}}.{{area.rep}}<img style="width:32px;height:32px" width="32px" height="32px" src="../assets/logo.png" alt="WildFyre"></mat-option>
   </mat-select>
   <button id="topNavMenu" mat-button (click)="sidenav.open()"><i class="material-icons">menu</i></button>
@@ -46,6 +46,9 @@
     <mat-nav-list>
       <a mat-list-item href="https://wildfyre.net/index.php/about-us/">
         About Us
+      </a>
+      <a mat-list-item href="https://phabricator.wildfyre.net">
+        Open Source!
       </a>
       <a mat-list-item href="https://wildfyre.net/index.php/frequently-asked-questions/">
         FAQ

src/app/navBar/navBar.component.ts
@@ -45,6 +45,7 @@ export class NavBarComponent implements OnInit, OnDestroy {
   styleHeightTextarea: string;
   styleMobile: string;
   stylePage: boolean;
+  width: string;
 
   constructor(
     private cdRef: ChangeDetectorRef,
@@ -327,6 +328,7 @@ export class NavBarComponent implements OnInit, OnDestroy {
   }
 
   setActiveIndex(s: string) {
+    this.width = '100%';
     if (s === undefined) {
       s = '/';
     }
@@ -336,10 +338,12 @@ export class NavBarComponent implements OnInit, OnDestroy {
       this.navBarService.areaVisible.next(false);
     } else if (s === '/notifications/archive') {
       this.stylePage = false;
+      this.width = '90%';
       this.activeLinkIndex = 1;
       this.navBarService.areaVisible.next(true);
     } else if (s.lastIndexOf('/notifications/archive/') !== -1) {
       this.stylePage = false;
+      this.width = '90%';
       this.activeLinkIndex = 1;
       this.navBarService.areaVisible.next(true);
     } else if (s === '/notifications') {
@@ -351,8 +355,8 @@ export class NavBarComponent implements OnInit, OnDestroy {
       this.activeLinkIndex = 1;
       this.navBarService.areaVisible.next(false);
     } else if (s === '/') {
-      this.activeLinkIndex = 2;
       this.stylePage = true;
+      this.activeLinkIndex = 2;
       this.navBarService.areaVisible.next(true);
     } else if (s === '/posts') {
       this.stylePage = false;
@@ -360,16 +364,30 @@ export class NavBarComponent implements OnInit, OnDestroy {
       this.navBarService.areaVisible.next(true);
     } else if (s.lastIndexOf('/posts/') !== -1) {
       this.stylePage = false;
+      this.activeLinkIndex = 3;
       this.navBarService.areaVisible.next(true);
     } else if (s === '/create') {
       this.stylePage = false;
       this.activeLinkIndex = 4;
       this.navBarService.areaVisible.next(true);
+    } else if (s.lastIndexOf('/create/') !== -1) {
+      this.stylePage = false;
+      this.activeLinkIndex = 4;
+      this.navBarService.areaVisible.next(false);
+    } else if (s === '/drafts') {
+      this.stylePage = false;
+      this.width = '90%';
+      this.activeLinkIndex = 4;
+      this.navBarService.areaVisible.next(true);
     } else if (s.lastIndexOf('/areas/') !== -1) {
       this.stylePage = true;
+      this.width = '90%';
+      this.activeLinkIndex = 2;
       this.navBarService.areaVisible.next(false);
     } else if (s.lastIndexOf('/user/') !== -1) {
       this.stylePage = false;
+      this.width = '90%';
+      this.activeLinkIndex = 0;
       this.navBarService.areaVisible.next(false);
     }
     this.cdRef.detectChanges();

src/app/notification/notification.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="example-card">
   <button class="wideButtons" mat-button (click)="loadArchive()"><i class="material-icons">folder</i> Archive</button>
   <hr *ngIf="(notifications.length !== 0) && !loading">

src/app/notificationArchive/notificationArchive.component.html
@@ -17,7 +17,7 @@
       <font size="0.1">
         <ng-container *ngIf="!post.anonym"><i class="material-icons">visibility_on</i></ng-container>
         <ng-container *ngIf="post.anonym"><i class="material-icons">visibility_off</i></ng-container>
-        <ng-container *ngIf="imageArray[currentArea][i]"><i class="material-icons">image</i></ng-container>
+        <ng-container *ngIf="imageArray[currentArea][i] || post.image"><i class="material-icons">image</i></ng-container>
         <span class="mainText" innerHTML="{{post.text | slice:0:200 | marked: post}}"></span><ng-container *ngIf="post.text.length > 200">...</ng-container>
         <hr>
         {{post.created}}

src/app/profile/profile.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-menu #appMenu="matMenu">
   <button mat-menu-item (click)="viewProfile()"><mat-icon>public</mat-icon>View Public Profile</button>
   <button mat-menu-item (click)="openBioDialog()"><mat-icon>mode_edit</mat-icon>Bio</button>

src/app/recover/recover.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="register-card">
   <a class="logoLink" routerLink="/" routerLinkActive="active"><img class="logo" src="https://static.wildfyre.net/wildfyre.png" alt="WildFyre"></a>
   <mat-card-content>

src/app/recoverPassword/recoverPassword.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="register-card">
   <a class="logoLink" routerLink="/" routerLinkActive="active"><img class="logo" src="https://static.wildfyre.net/wildfyre.png" alt="WildFyre"></a>
   <mat-card-content>

src/app/register/register.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="register-card">
   <a class="logoLink" routerLink="/" routerLinkActive="active"><img class="logo" src="https://static.wildfyre.net/wildfyre.png" alt="WildFyre"></a>
   <mat-card-content>

src/app/registerSuccess/registerSuccess.component.html
@@ -1,4 +1,3 @@
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="example-card">
   <mat-card-content>
     <a routerLink="/" routerLinkActive="active"><img class="logo" src="https://static.wildfyre.net/wildfyre.png" alt="WildFyre"></a>

src/app/userPosts/userPosts.component.html
@@ -3,7 +3,6 @@
   <input matInput placeholder="Search Posts" type="text" name="search" (input)="searchInput()" [(ngModel)]="model.postText" #postText="ngModel" autocomplete="off" />
 </mat-input-container>
 -->
-<div id="topNavBackHidden"><i class="material-icons">arrow_back</i></div>
 <mat-card class="refreshButton" (click)="ngOnInit()" *ngIf="totalCount === 0 && !loading">
     No posts, try posting?
     <button class="wideButtons" mat-button>Refresh <i class="material-icons">refresh</i></button>
@@ -17,7 +16,7 @@
           <font size="0.1">
             <ng-container *ngIf="!post.anonym"><i class="material-icons">visibility_on</i></ng-container>
             <ng-container *ngIf="post.anonym"><i class="material-icons">visibility_off</i></ng-container>
-            <ng-container *ngIf="imageArray[currentArea][i]"><i class="material-icons">image</i></ng-container>
+            <ng-container *ngIf="imageArray[currentArea][i] || post.image"><i class="material-icons">image</i></ng-container>
             <span class="mainText" innerHTML="{{post.text | slice:0:200 | marked: post}}"></span><ng-container *ngIf="post.text.length > 200">...</ng-container>
             <hr>
             {{post.created}}

src/styles.css
@@ -8,6 +8,10 @@ a.mat-tab-link {
   opacity: 1.0;
 }
 
+body {
+  background: #fafafa;
+}
+
 button[type="submit"] {
     white-space: normal;
     width: 100%;
@@ -96,6 +100,10 @@ hr {
   background-color: #333; /* Modern Browsers */
 }
 
+html {
+  background: #fafafa;
+}
+
 img {
   height: 100%;
   width: 100%;
@@ -136,7 +144,7 @@ mat-select {
 
 mat-select div.mat-select-trigger {
   width: 200px;
-  margin-top: -5px;
+  margin-top: 11px;
   padding-bottom: 4px;
   border-bottom: 2px solid #ed763e;
 }
@@ -148,6 +156,7 @@ mat-sidenav.sideNav {
   position: fixed;
   right: 0px;
   top: 0px;
+  padding-top: 85px;
 }
 
 mat-sidenav-container.mat-drawer-container.mat-sidenav-container.mat-drawer-transition {
@@ -182,6 +191,10 @@ span {
 
 span.mat-button-wrapper {
   display: contents;
+}
+
+span.markdown p a {
+  display: flex;
 }
 
 span.markdown table {
@@ -240,10 +253,6 @@ ul.ngx-pagination {
   width: 100%;
   text-align: center;
   margin: 0 auto;
-}
-
-.areaSelect {
-  padding-right: 10%;
 }
 
 .brick {
@@ -356,6 +365,10 @@ ul.ngx-pagination {
   text-align: left;
   width: 100%;
   margin: 0 auto;
+}
+
+.mat-form-field-required-marker {
+  display: contents;
 }
 
 .mat-raised-button:hover {
@@ -499,6 +512,11 @@ ul.ngx-pagination {
   float: right;
 }
 
+#areaSelect {
+  padding-right: 10%;
+  display: contents;
+}
+
 #captcha {
   margin: 0 auto;
   width: 304px;
@@ -514,6 +532,8 @@ ul.ngx-pagination {
   border-left: 6px solid #263238;
   padding-bottom: 5vh;
   margin-bottom: 2vh;
+  text-align: left;
+  padding-left: 5px;
 }
 
 #focusTouchZone {
@@ -533,7 +553,7 @@ ul.ngx-pagination {
   position: fixed;
   top: 0px;
   right: 0px;
-  width: 90%;
+  width: 100%;
   z-index: 5;
 }
 
@@ -555,6 +575,7 @@ ul.ngx-pagination {
   display: flex;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
 }
 
 #topNavBackHidden {
@@ -729,7 +750,7 @@ ul.ngx-pagination {
       bottom: 0px;
       z-index: 3;
       width: 7%;
-      height: 57px;
+      height: 56px;
       background-color: #263238;
       color: white;
       text-align: center;
@@ -877,7 +898,7 @@ ul.ngx-pagination {
 
     #navB {
       position: fixed;
-      top: 61px;
+      top: 59px;
       width: 100%;
       float: left;
       z-index: 5;